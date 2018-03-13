c0c46f35d4d3d163acf445e7e5de114ff9a14221
Vincent Letourneau <vincent@nanoninja.com>
2018-03-13 09:57:44 +0100

    Update

--- bases/types/func.go
+++ bases/types/func.go
@@ -1,7 +1,36 @@
 package main
 
-// Les fonctions :D
-// toi aussi tu peux créer ce fichier.
-func main() {
+import "fmt"
 
+func GetNames() (string, string) {
+	return "John", "Doe"
+}
+
+func ShowNames(firstname, lastname string) {
+	fmt.Println(firstname, lastname)
+}
+
+type Funky func()
+
+func (f Funky) Dance() {
+	f()
+}
+
+type Dancer interface {
+	Dance()
+}
+
+func StartParty(d Dancer) {
+	d.Dance()
+}
+
+func main() {
+	ShowNames(GetNames())
+
+	var f Funky
+	f = func() {
+		fmt.Println("I'm dancing, I Got Funky")
+	}
+
+	StartParty(f)
 }
