168c82dc48d851acd81ee4ec7702fc3ea292b0ed
Vincent Letourneau <vincent@nanoninja.com>
2018-03-15 17:41:20 +0100

    Update

--- api/controller/task.go
+++ api/controller/task.go
@@ -5,6 +5,7 @@ import (
 	"api/model"
 	"api/store"
 	"net/http"
+	"strconv"
 )
 
 type Task struct {
@@ -29,11 +30,42 @@ func NewTask(s store.Manager) *Task {
 			Pattern: t.Prefix,
 			Func:    t.Create,
 		},
+		model.Route{
+			Name:    "All",
+			Method:  "GET",
+			Pattern: t.Prefix,
+			Func:    t.All,
+		},
 	}
 
 	t.Routes = append(t.Routes, routes...)
 
 	return t
+}
+
+func (t *Task) All(w http.ResponseWriter, r *http.Request) {
+	var err error
+
+	offset := store.NoPaging
+	limit := store.NoPaging
+
+	offset, err = strconv.Atoi(handler.GetParams("offset", r))
+	if err != nil {
+		offset = store.NoPaging
+	}
+
+	limit, err = strconv.Atoi(handler.GetParams("limit", r))
+	if err != nil {
+		limit = store.NoPaging
+	}
+
+	tasks, err := t.store.All(offset, limit)
+	if err != nil {
+		handler.SendJSONError(w, "Error All", http.StatusInternalServerError)
+		return
+	}
+
+	handler.SendJSONOk(w, &tasks)
 }
 
 func (t *Task) Create(w http.ResponseWriter, r *http.Request) {
